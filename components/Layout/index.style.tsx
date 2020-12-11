13973faaae33b1d3448f7005ab0d3982d5bf3353
Zeus3hd <ahdhani0@gmail.com>
2020-12-12 02:47:35 +0300

    create half circle

--- components/Layout/index.style.tsx
+++ components/Layout/index.style.tsx
@@ -10,6 +10,14 @@ export const BackgroundWrapper = styled.div`
   height: 100vh;
   background: ${({ theme: { colors } }) => colors.offwhite};
   z-index: -1;
+  > svg {
+    position: absolute;
+    height: 20%;
+    stroke-width: 6px;
+    left: 50%;
+    top: 70%;
+    stroke: ${({ theme: { colors } }) => colors.lightPurple};
+  }
 `;
 export const BackgroundVerticalLine = styled.div<{ num: number }>`
   height: 100%;
@@ -46,6 +54,12 @@ export const BackgroundSquare = styled.div<{
   position: absolute;
 `;
 
+const HalfCircle = () => (
+  <svg viewBox="0 0 83 83" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M77 41.5C77 48.5212 74.918 55.3848 71.0172 61.2227C67.1164 67.0607 61.572 71.6108 55.0853 74.2977C48.5985 76.9846 41.4606 77.6877 34.5743 76.3179C27.688 74.9481 21.3625 71.5671 16.3977 66.6023C11.4329 61.6375 8.0519 55.312 6.68212 48.4257C5.31235 41.5394 6.01537 34.4015 8.70228 27.9147C11.3892 21.428 15.9393 15.8836 21.7773 11.9828C27.6152 8.08204 34.4788 6 41.5 6" />
+  </svg>
+);
+
 export const Background = () => {
   return (
     <BackgroundWrapper>
@@ -60,6 +74,7 @@ export const Background = () => {
       <BackgroundSquare top={90} left={10} />
       <BackgroundSquare top={90} left={90} purple />
       <BackgroundSquare top={90} left={10} />
+      <HalfCircle />
     </BackgroundWrapper>
   );
 };
